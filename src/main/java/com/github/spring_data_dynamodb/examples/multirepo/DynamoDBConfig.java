651c68ab5f0913380b634570b9520e22c240b1b5
mp-loki <valerii.sosliuk@gmail.com>
2018-04-08 23:06:09 -0400

    Minor changes to make it run with local DynamoDB

--- src/main/java/com/github/spring_data_dynamodb/examples/multirepo/DynamoDBConfig.java
+++ src/main/java/com/github/spring_data_dynamodb/examples/multirepo/DynamoDBConfig.java
@@ -1,6 +1,7 @@
 package com.github.spring_data_dynamodb.examples.multirepo;
 
 import com.amazonaws.auth.*;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -8,9 +9,13 @@ import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRep
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class DynamoDBConfig {
+    @Value("${amazon.dynamodb.endpoint}")
+    private String amazonDynamoDBEndpoint;
+
     @Value("${amazon.aws.accesskey}")
     private String amazonAWSAccessKey;
 
@@ -28,9 +33,14 @@ public class DynamoDBConfig {
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(amazonAWSCredentialsProvider())
-                .withRegion(Regions.US_EAST_1)
-                .build();
+        AmazonDynamoDBClientBuilder builder =  AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(amazonAWSCredentialsProvider());
+
+        if (!StringUtils.isEmpty(amazonDynamoDBEndpoint)) {
+            builder.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, Regions.US_EAST_1.toString()));
+        } else {
+            builder.withRegion(Regions.US_EAST_1);
+        }
+        return builder.build();
     }
 }
